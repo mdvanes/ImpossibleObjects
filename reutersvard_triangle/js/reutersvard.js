1323a4a49ccc7c22bedd765e8e35f9e0044ec448
m.van.es <m.van.es@everest.nl>
2015-01-31 18:44:49 +0100

    added reutersvard

--- reutersvard_triangle/js/reutersvard.js
+++ reutersvard_triangle/js/reutersvard.js
@@ -6,11 +6,11 @@
     };
 
     var Cube = function(ctx, left, top) {
-        console.log('new cube ', left, top);
+        //console.log('new cube ', left, top);
         //ctx.fillRect(left, top, 100, 100);
 
         var width = settings.width;
-        var offset = 50;
+        var offset = settings.width / 2;
 
         var topPlane = new Path2D();
         topPlane.moveTo(left, top);
@@ -66,7 +66,9 @@
         // var ribsRightCenters = ribsRight.map(function(index) {
 
         // });
-        var ribsRight, ribsBottom, cubeCenters, i;
+        var ribsRight, ribsBottom, ribsLeft, cubeCenters, i;
+        cubeCenters = [];//[[300, 0],[300 + horizontalDistance, verticalDistance],[300 + (horizontalDistance * 2), verticalDistance * 2]];
+
         ribsRight = [];
         for(i = 0; i < 4; i++) {
             //var coords = [];
@@ -74,24 +76,70 @@
             var y = verticalDistance * i;
             ribsRight.push([x,y]);
         }
-
-        cubeCenters = [];//[[300, 0],[300 + horizontalDistance, verticalDistance],[300 + (horizontalDistance * 2), verticalDistance * 2]];
         cubeCenters = cubeCenters.concat(ribsRight);
 
         ribsBottom = [];
-        for(i = 0; i < 4; i++) {
+        for(i = 1; i < 4; i++) {
             // TODO scope
-            var x1 = (300 + (horizontalDistance * 4)) - (horizontalDistance * (i+1));
+            var x1 = (300 + (horizontalDistance * 3)) - (horizontalDistance * i * 2);
+            //console.log('ribsbottom', x1);
             var y1 = (verticalDistance * 3);
             ribsBottom.push([x1,y1]);
         }
         cubeCenters = cubeCenters.concat(ribsBottom);
 
+        ribsLeft = [];
+        for(i = 2; i > 0; i--) {
+            // TODO scope
+            var x2 = 300 - (horizontalDistance * i);
+            //console.log('ribsbottom', x1);
+            var y2 = verticalDistance * i;
+            ribsLeft.push([x2,y2]);
+        }
+        cubeCenters = cubeCenters.concat(ribsLeft);
+
         //console.log('cub',cubeCenters);
         cubeCenters.map(function(coords) {
-            console.log(coords);
+            //console.log(coords);
             new Cube(ctx, coords[0], coords[1]);
         });
+
+        // TODO to function
+        // TODO to relative
+        var left = 300;
+        var top = 0;
+        var offset = settings.width / 2;
+        var extraLeftPlane = new Path2D();
+        // TODO reuse from Cube
+        extraLeftPlane.moveTo(left, top);
+        extraLeftPlane.lineTo(left + offset, top + 90);
+        extraLeftPlane.lineTo(left, top + 180);
+        extraLeftPlane.lineTo(left - offset, top + 90);
+
+        ctx.fillStyle = '#FF6347';
+        ctx.fill(extraLeftPlane);
+
+        var extraRightPlane = new Path2D();
+        extraRightPlane.moveTo(left + offset, top + 90);
+        extraRightPlane.lineTo(left + (settings.width/2) + offset, top + 90);
+        extraRightPlane.lineTo(left + (settings.width/2), top + 180);
+        extraRightPlane.lineTo(left, top + 180);
+
+        ctx.fillStyle = '#008080';
+        ctx.fill(extraRightPlane);
+
+        // TODO reuse ribsRight function?
+        left = 300 + horizontalDistance;
+        top = verticalDistance;
+        offset = settings.width / 2;
+        var extraLeftPlane2 = new Path2D();
+        extraLeftPlane2.moveTo(left, top);
+        extraLeftPlane2.lineTo(left + offset, top + 90);
+        extraLeftPlane2.lineTo(left, top + 180);
+        extraLeftPlane2.lineTo(left - offset, top + 90);
+
+        ctx.fillStyle = '#FF6347';
+        ctx.fill(extraLeftPlane2);
     };
 
     $(document).ready(function(){
